6a3a85feeec4552219af964ebc961107e973bdc7
GerasimenkoAlexander <gerasimenkoalexanderua@gmail.com>
2018-11-23 23:59:26 +0200

    Create Layout boilerplater

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,15 @@
-export default () => <div>Welcome to next.js!</div>
+import { Component } from "react";
+​
+import Layout from "../components/Layout";
+​
+class Job extends Component {
+​
+    render() {
+        return (
+            <Layout>
+                <div>Hello World</div>
+            </Layout>
+        );
+    }
+}
+​
